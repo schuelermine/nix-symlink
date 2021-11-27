1d4c24b446fcbbbd12c3fd13ecc4e516e4b7d48c
Anselm Schüler <mail@anselmschueler.com>
2021-11-27 20:51:50 +0100

    Format with nixfmt

--- symlink.nix
+++ symlink.nix
@@ -2,7 +2,9 @@
 , link, target, link-name ? link, target-name ? target }:
 derivation {
   system = system;
-  name = "symlink-${builtins.baseNameOf link-name}-${builtins.baseNameOf target-name}";
+  name = "symlink-${builtins.baseNameOf link-name}-${
+      builtins.baseNameOf target-name
+    }";
   builder = "${busybox}/bin/sh";
   args = [ ./symlink.sh ];
   dir = let dir = builtins.dirOf link; in if dir == "." then "" else dir;
