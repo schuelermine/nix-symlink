1efdfecc9ddd3508ef451fb53e6a323329f3a843
Anselm Schüler <mail@anselmschueler.com>
2021-11-27 20:43:50 +0100

    Add link-name and target-name parameters
    This is useful if the target is in a derivation, because the name cannot depend on the derivation

--- symlink.nix
+++ symlink.nix
@@ -1,8 +1,8 @@
 { busybox ? (import <nixpkgs> { }).busybox, system ? builtins.currentSystem
-, link, target }:
+, link, target, link-name ? link, target-name ? target }:
 derivation {
   system = system;
-  name = "symlink-${builtins.baseNameOf link}-${builtins.baseNameOf target}";
+  name = "symlink-${builtins.baseNameOf link-name}-${builtins.baseNameOf target-name}";
   builder = "${busybox}/bin/sh";
   args = [ ./symlink.sh ];
   dir = let dir = builtins.dirOf link; in if dir == "." then "" else dir;
