bf0d8a3f8e714a9de102aa4704bf83e68225740d
Anselm Schüler <mail@anselmschueler.com>
2021-12-26 12:32:38 +0100

    add shell param

--- symlink.nix
+++ symlink.nix
@@ -1,11 +1,12 @@
-{ system ? builtins.currentSystem, utils ? (import <nixpkgs> { }).busybox, link
-, target, link-label ? link, target-label ? target }:
+{ system ? builtins.currentSystem, utils ? (import <nixpkgs> { }).busybox
+, shell ? "${(import <nixpkgs> { }).busybox}/bin/sh", link, target
+, link-label ? link, target-label ? target }:
 derivation {
   system = system;
   name = "symlink-${builtins.baseNameOf link-label}-${
       builtins.baseNameOf target-label
     }";
-  builder = "${utils}/bin/sh";
+  builder = shell;
   args = [ ./symlink.sh ];
   dir = let dir = builtins.dirOf link; in if dir == "." then "" else dir;
   inherit link target utils;
